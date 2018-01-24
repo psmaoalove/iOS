a091e27afece3b618e46c98e98780ce8b9a8f836
csdnliming <25865953@qq.com>
2018-01-24 18:49:48 +0800

    first commit

--- ooo/AppDelegate.m
+++ ooo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor=[UIColor whiteColor];
+    ViewController * viewcon=[[ViewController alloc]init];
+    [self.window setRootViewController:viewcon];
+    [self.window makeKeyAndVisible];
+   
+    
+    
     return YES;
 }
 
--- ooo/ViewController.m
+++ ooo/ViewController.m
@@ -13,13 +13,51 @@
 @end
 
 @implementation ViewController
+-(void)viewWillAppear:(BOOL)animated
+
+{
+    [super viewWillAppear:animated];
+    //    [self.navigationController setToolbarHidden:NO animated:YES];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIViewController *vc1 = [[UIViewController alloc] init];
+    vc1.navigationItem.title=@"通讯";
+    vc1.view.backgroundColor = [UIColor blueColor];
+    
+    UIViewController *vc2 = [[UIViewController alloc] init];
+    vc2.navigationItem.title=@"朋友";
+    vc2.view.backgroundColor = [UIColor redColor];
+    
+    UIViewController *vc3 = [[UIViewController alloc] init];
+    vc3.navigationItem.title= @"记录";
+    vc3.view.backgroundColor = [UIColor greenColor];
+    
+    
+    UINavigationController *firstNC = [[UINavigationController alloc]initWithRootViewController:vc1];
+    firstNC.tabBarItem.title=@"通讯录";
+
+
+    UINavigationController *secondNC = [[UINavigationController alloc]initWithRootViewController:vc2];
+    secondNC.tabBarItem.title=@"朋友圈";
+    
+    UINavigationController *thridNC = [[UINavigationController alloc]initWithRootViewController:vc3];
+    thridNC.tabBarItem.title=@"通话记录";
+    
+    NSArray *array = @[firstNC,secondNC,thridNC];
+    
+    self.viewControllers=array;
+    [self.tabBar setBarTintColor:[UIColor whiteColor]];
+    self.tabBar.tintColor = [UIColor blackColor];
+    //    self.tabBar.barStyle = UIBarStyleBlack;
+    self.selectedIndex = 0;
+
+    
+    
 }
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
